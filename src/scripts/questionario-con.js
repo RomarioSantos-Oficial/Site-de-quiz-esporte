de78266d41e3dbf518ba33117b1a333a1233accf
RomarioSantos-Oficial <limaromario40@gmail.com>
2024-02-18 16:35:18 -0300

    este001

---
+++ src/scripts/questionario-con.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+      question: "Qual foi o ano de fundação do Sport Club Corinthians Paulista?",
+      options: ["1909", "1910", "1912", "1920"],
+      answer: "1910",
+      explanation: "O Sport Club Corinthians Paulista foi fundado em 1º de setembro de 1910, por um grupo de operários do bairro Bom Retiro, em São Paulo."
+    },
+    {
+      question: "Qual jogador é conhecido como 'Doutor' e é uma das maiores lendas do Corinthians?",
+      options: ["Rivellino", "Sócrates", "Marcelinho Carioca", "Ronaldo"],
+      answer: "Sócrates",
+      explanation: "Sócrates é conhecido como 'Doutor' e é uma das maiores lendas do Corinthians. Ele foi um jogador icônico, tanto pelo seu talento em campo quanto por seu ativismo político e suas ideias progressistas."
+    },
+    {
+      question: "Quantos títulos brasileiros o Corinthians conquistou até 2022?",
+      options: ["5", "6", "7", "8"],
+      answer: "7",
+      explanation: "Até 2022, o Corinthians conquistou sete títulos brasileiros. O clube é um dos mais bem-sucedidos do país nessa competição, tendo ganho o campeonato em várias ocasiões ao longo de sua história."
+    },
+    {
+      question: "Qual é o mascote oficial do Corinthians?",
+      options: ["Mosqueteiro", "Gavião", "Lobo", "Raposa"],
+      answer: "Mosqueteiro",
+      explanation: "O Mosqueteiro oficial do Corinthians é o Gavião. Ele representa a força, a agilidade e a determinação do clube em suas conquistas."
+    },
+    {
+      question: "Quem foi o técnico do Corinthians na conquista da Copa Libertadores da América em 2012?",
+      options: ["Mano Menezes", "Tite", "Adenor Bacchi", "Oswaldo de Oliveira"],
+      answer: "Tite",
+      explanation: "Tite foi o técnico responsável por liderar o Corinthians à histórica conquista da Copa Libertadores da América em 2012. Sua abordagem tática e seu estilo de jogo disciplinado foram fundamentais para o sucesso da equipe."
+    },
+    {
+      question: "Qual jogador marcou o gol do título do Mundial de Clubes da FIFA de 2012 pelo Corinthians?",
+      options: ["Paolo Guerrero", "Romarionho", "Paulinho", "Danilo"],
+      answer: "Paolo Guerrero",
+      explanation: "Paolo Guerrero foi o jogador que marcou o gol do título do Mundial de Clubes da FIFA de 2012 pelo Corinthians."
+    },
+    {
+      question: "Qual é o estádio principal do Corinthians?",
+      options: ["Neo Química Arena", "Arena Corinthians", " Arena Neo Química", "Estádio Nacional de Brasília"],
+      answer: "Neo Química Arena",
+      explanation: "A Neo Química Arena, também conhecida como Itaquerão, é o estádio principal do Corinthians. Localizado em São Paulo, foi inaugurado em 2014 e tem capacidade para mais de 49 mil torcedores."
+    },
+    {
+      question: "Qual jogador do Corinthians foi artilheiro do Campeonato Brasileiro de 2017?",
+      options: ["Jô", "Romarinho", "Renato Augusto", "Paulinho"],
+      answer: "Jô",
+      explanation: "Jô jutamente com Rodrigo  Dourado do Fluminense foi o artilheiro do Campeonato Brasileiro de 2017, ajudando o Corinthians a conquistar mais um título nacional."
+    },
+    {
+      question: "Qual foi o a soma do placar da final da Copa Libertadores da América de 2012, na qual o Corinthians venceu?",
+      options: ["1-0", "2-0", "3-1", "2-1"],
+      answer: "3-1",
+      explanation: "O placar da final da Copa Libertadores da América de 2012, na qual o Corinthians venceu o Boca Juniors, foi 2-0 em casa e primeiro jogo foi 1-1 na Agentina. No agragado foi de 3-1 paro o Corinthians"
+    },
+  ];
+  
